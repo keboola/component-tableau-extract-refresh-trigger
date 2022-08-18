c461bb2e747a8c8ee4aaf65938e51dfa1b41a11c
František Navrkal <frantisek@revolt.bi>
2022-08-18 18:13:19 +0000

    First draft of implementation

---
+++ src/client.py
@@ -0,0 +1,261 @@
+from dataclasses import dataclass, field
+from enum import Enum
+from math import sqrt
+from typing import List, Optional
+import logging
+
+from retry import retry
+
+from keboola.component.exceptions import UserException
+
+import tableauserverclient as tsc
+
+from tableauserverclient.models import (
+    TaskItem,
+    DatasourceItem,
+    ServerInfoItem,
+    JobItem,
+    Target,
+)
+from tableauserverclient.server import RequestOptions
+from tableauserverclient.server.endpoint import ServerResponseError
+
+KEY_NAME = "name"
+KEY_TYPE = "type"
+KEY_TAG = "tag"
+KEY_LUID = "luid"
+
+REQUEST_MAX_RETRIES = 42
+REQUEST_RETRIES_INITIAL_DELAY = 0.5
+REQUEST_RETRIES_BACKOFF_FACTOR = sqrt(2)
+REQUEST_RETRIES_MAX_DELAY = 60
+REQUEST_RETRIES_JITTER = (0, 0.5)
+
+
+class DatasourceRefreshTaskType(Enum):
+    Full = TaskItem.Type.ExtractRefresh
+    Incremental = "IncrementExtractTask"
+
+    @classmethod
+    def from_string(cls, value: str) -> Optional["DatasourceRefreshTaskType"]:
+        for item in cls:
+            if item.value == value:
+                return item
+        if value == "RefreshExtractTask":
+            return cls.Full
+        raise ValueError(f"{value} is not a valid DatasourceRefreshTaskType")
+
+
+@dataclass(slots=True, frozen=True)
+class DatasourceRefreshSpec:
+    name: str
+    tag: str
+    type: DatasourceRefreshTaskType
+    luid: str
+
+    @classmethod
+    def from_dict(cls, d: dict) -> "DatasourceRefreshSpec":
+        return cls(
+            name=d[KEY_NAME],
+            tag=d[KEY_TAG],
+            type=DatasourceRefreshTaskType.from_string(d[KEY_TYPE]),
+            luid=d[KEY_LUID],
+        )
+
+    def to_dict(self) -> dict:
+        return {
+            KEY_NAME: self.name,
+            KEY_TAG: self.tag,
+            KEY_TYPE: self.type.value,
+            KEY_LUID: self.luid,
+        }
+
+    def __str__(self) -> str:
+        return str(self.to_dict())
+
+
+def datasource_to_string(datasource: DatasourceItem) -> str:
+    return (
+        f"(Name: {datasource.name}, Project:{datasource.project_name},"
+        f" LUID: {datasource.id}, Tags: {datasource.tags})"
+    )
+
+
+def datasource_list_to_string(datasources: List[DatasourceItem]) -> str:
+    str = "["
+    for ds in datasources:
+        str += f"{datasource_to_string(ds)}, "
+    str += "]"
+    return str
+
+
+def task_list_to_string(tasks: List[TaskItem]) -> str:
+    str = "["
+    for task in tasks:
+        str += f"(ID: {task.id}, Task type:{task.task_type}, Target: {task.target}), "
+    str += "]"
+    return str
+
+
+def generate_datasource_search_error_message(
+    spec: DatasourceRefreshSpec, datasources: List[DatasourceItem]
+) -> str:
+    error_message = None
+    if not datasources:
+        if not spec.luid:
+            error_message = f"There is no data source for the combination of name {spec.name} and tag {spec.tag}."
+        if spec.luid:
+            error_message = (
+                f"There is no data source with the specified LUID {spec.luid}."
+            )
+
+    # this happens when luid is set and name is not matching the dataset
+    if len(datasources) == 1 and spec.name != datasources[0].name:
+        error_message = (
+            f"The dataset name retrieved by the specified LUID: '{datasources[0].name}' "
+            f"does not match the '{spec.name}' specified in corresponding filter: {spec}"
+        )
+
+    if len(datasources) > 1:
+        error_message = (
+            f"There is more results for given filter: {spec}, "
+            f"set more specific tag or use LUID. The results are: {datasource_list_to_string(datasources)}"
+        )
+    return error_message
+
+
+@dataclass(slots=True)
+class TableauServerClient:
+    token_name: str
+    token_secret: str
+    site_id: Optional[str]
+    endpoint: str
+    __auth: tsc.PersonalAccessTokenAuth = field(init=False)
+    __server: tsc.Server = field(init=False)
+    __server_info: ServerInfoItem = field(init=False)
+    __request_retry_decorator = retry(
+        exceptions=ServerResponseError,
+        tries=REQUEST_MAX_RETRIES,
+        delay=REQUEST_RETRIES_INITIAL_DELAY,
+        backoff=REQUEST_RETRIES_BACKOFF_FACTOR,
+        max_delay=REQUEST_RETRIES_MAX_DELAY,
+        jitter=REQUEST_RETRIES_JITTER,
+        logger=logging.getLogger(),
+    )
+    __all_tasks: Optional[List[TaskItem]] = field(init=False, default=None)
+
+    def __post_init__(self):
+        # if authentication_type == "user/password":
+        # self.auth = tsc.TableauAuth(
+        #     self.cfg_params[KEY_USER_NAME],
+        #     self.cfg_params[KEY_API_PASS],
+        #     site_id=site_id,
+        # )
+        self.__auth = tsc.PersonalAccessTokenAuth(
+            token_name=self.token_name,
+            personal_access_token=self.token_secret,
+            site_id=self.site_id,
+        )
+        self.__server = tsc.Server(self.endpoint, use_server_version=True)
+        self.__server_info = self.__server.server_info.get()
+        logging.info(f"Using server API version: {self.__server_info.rest_api_version}")
+
+    @__request_retry_decorator
+    def get_datasources(
+        self, req_options: Optional[RequestOptions] = None
+    ) -> List[DatasourceItem]:
+        with self.__server.auth.sign_in(self.__auth):
+            return list(tsc.Pager(self.__server.datasources, request_opts=req_options))
+
+    @__request_retry_decorator
+    def get_tasks(self, req_options: Optional[RequestOptions] = None) -> List[TaskItem]:
+        with self.__server.auth.sign_in(self.__auth):
+            return list(tsc.Pager(self.__server.tasks, request_opts=req_options))
+
+    @__request_retry_decorator
+    def get_datasource_by_id(self, datasource_id: str) -> DatasourceItem:
+        with self.__server.auth.sign_in(self.__auth):
+            return self.__server.datasources.get_by_id(datasource_id)
+
+    @__request_retry_decorator
+    def wait_for_job(self, job_id: str | JobItem) -> JobItem:
+        with self.__server.auth.sign_in(self.__auth):
+            return self.__server.jobs.wait_for_job(job_id)
+
+    @__request_retry_decorator
+    def run_task(self, task_item: TaskItem) -> JobItem:
+        with self.__server.auth.sign_in(self.__auth):
+            response_content = self.__server.tasks.run(
+                task_item
+            )  # This library function is unfinished so I have to explicitly parse the response:
+        job_items = JobItem.from_response(response_content, self.__server.namespace)
+        assert len(job_items) == 1
+        return job_items[0]
+
+    def get_datasource_by_name_and_tag(
+        self, name: str, tag: Optional[str]
+    ) -> List[DatasourceItem]:
+        req_options = tsc.RequestOptions()
+        req_options.filter.add(
+            tsc.Filter(
+                tsc.RequestOptions.Field.Name, tsc.RequestOptions.Operator.Equals, name
+            )
+        )
+        if tag:
+            req_options.filter.add(
+                tsc.Filter(
+                    tsc.RequestOptions.Field.Tags,
+                    tsc.RequestOptions.Operator.Equals,
+                    tag,
+                )
+            )
+        return self.get_datasources(req_options=req_options)
+
+    def get_task_by_datasource_refresh_spec(
+        self, spec: DatasourceRefreshSpec
+    ) -> TaskItem:
+        if self.__all_tasks is None:
+            self.__all_tasks = self.get_tasks()
+        # Find the datasource by LUID or name and tag:
+        if spec.luid:
+            ds = self.get_datasource_by_id(spec.luid)
+        else:
+            candidate_ds_list = self.get_datasource_by_name_and_tag(spec.name, spec.tag)
+            if len(candidate_ds_list) != 1:
+                raise UserException(
+                    generate_datasource_search_error_message(spec, candidate_ds_list)
+                )
+            ds = candidate_ds_list[0]
+        tasks_found: List[TaskItem] = list()
+        for task in self.__all_tasks:
+            if task.task_type != spec.type.value:
+                continue
+            target: Target = task.target
+            if target.type != "datasource":
+                continue
+            if target.id != ds.id:
+                continue
+            tasks_found.append(task)
+        if len(tasks_found) > 1:
+            raise UserException(
+                f"Found {len(tasks_found)} tasks matching spec {spec}."
+                f" The results are: {task_list_to_string(tasks_found)}"
+            )
+        elif len(tasks_found) == 0:
+            UserException(
+                f"No task found for dataset: {datasource_to_string(ds)}."
+                f" Please create the extract refresh of type {spec.type} first."
+            )
+        return tasks_found[0]
+
+    def refresh_datasources(
+        self, ds_list: List[DatasourceRefreshSpec], wait_for_jobs: bool = False
+    ) -> List[JobItem]:
+        tasks_to_run = [
+            self.get_task_by_datasource_refresh_spec(spec) for spec in ds_list
+        ]
+        jobs = [self.run_task(task) for task in tasks_to_run]
+        if wait_for_jobs:
+            return [self.wait_for_job(job) for job in jobs]
+        else:
+            return jobs
--- src/component.py
+++ src/component.py
@@ -1,251 +1,101 @@
-'''
+"""
 Template Component main class.
 
-'''
+"""
 
 import logging
-import os
-import sys
+from typing import List, Optional
 
-import tableauserverclient as tsc
-import xmltodict
-from kbc.env_handler import KBCEnvHandler
+# import xmltodict
+
+from keboola.component import ComponentBase, UserException
+
+from client import TableauServerClient, DatasourceRefreshSpec
 
 # global constants
 
 
 # configuration variables
-KEY_TAG = 'tag'
-KEY_NAME = 'name'
-KEY_LUID = 'luid'
-KEY_API_PASS = '#password'
-KEY_USER_NAME = 'user'
-KEY_ENDPOINT = 'endpoint'
-KEY_POLL_MODE = 'poll_mode'
-KEY_DS_NAME = 'name'
-KEY_DS_TYPE = 'type'
-KEY_DATASOURCES = 'datasources'
-KEY_SITE_ID = 'site_id'
+# KEY_API_PASS = "#password"
+# KEY_USER_NAME = "user"
+KEY_ENDPOINT = "endpoint"
+KEY_POLL_MODE = "poll_mode"
 
-KEY_AUTH_TYPE = 'authentication_type'
-MANDATORY_PARS = [KEY_API_PASS, KEY_USER_NAME, KEY_DATASOURCES, KEY_ENDPOINT]
+KEY_DATASOURCES = "datasources"
+KEY_SITE_ID = "site_id"
+KEY_TOKEN_NAME = "token_name"
+KEY_TOKEN_SECRET = "#token_secret"
 
-APP_VERSION = '0.0.1'
+KEY_AUTH_TYPE = "authentication_type"
+MANDATORY_PARS = [
+    KEY_AUTH_TYPE,
+    KEY_TOKEN_NAME,
+    KEY_TOKEN_SECRET,
+    KEY_DATASOURCES,
+    KEY_ENDPOINT,
+]
 
 
-class Component(KBCEnvHandler):
+class TableauExtractRefreshTrigger(ComponentBase):
+    def run(self):
+        """
+        Main execution code
+        """
 
-    def __init__(self, debug=False):
-        KBCEnvHandler.__init__(self, MANDATORY_PARS, )
+        params: dict = self.configuration.parameters  # noqa
+
+        authentication_type: str = params.get(KEY_AUTH_TYPE, "user/password")
+        if authentication_type != "Personal Access Token":
+            raise UserException("Personal Access Token must be used.")
+
+        # self.validate_configuration_parameters(MANDATORY_PARS)
         # override debug from config
-        if self.cfg_params.get('debug'):
-            debug = True
-
-        log_level = logging.DEBUG if debug else logging.INFO
-        # setup GELF if available
-        if os.getenv('KBC_LOGGER_ADDR', None):
-            self.set_gelf_logger(log_level)
-        else:
-            self.set_default_logger(log_level)
-        logging.info('Running version %s', APP_VERSION)
-        logging.info('Loading configuration...')
+        debug: Optional[bool] = params.get("debug")
 
         if not debug:
             # suppress info logging on the Tableau endpoints
-            logging.getLogger('tableau.endpoint.jobs').setLevel(logging.ERROR)
-            logging.getLogger('tableau.endpoint.datasources').setLevel(logging.ERROR)
+            logging.getLogger("tableau.endpoint.jobs").setLevel(logging.ERROR)
+            logging.getLogger("tableau.endpoint.datasources").setLevel(logging.ERROR)
 
-        try:
-            self.validate_config()
-        except ValueError as e:
-            logging.exception(e)
-            exit(1)
+        token_name: str = params[KEY_TOKEN_NAME]
+        token_secret: str = params[KEY_TOKEN_SECRET]
+        endpoint: str = params[KEY_ENDPOINT]
+        datasource_refresh_specs: List[dict] = params[KEY_DATASOURCES]
 
-        site_id = self.cfg_params.get(KEY_SITE_ID) or ''
-        # intialize instance parameteres
+        site_id: str = params.get(KEY_SITE_ID)
+        wait_for_jobs: bool = bool(params.get(KEY_POLL_MODE, False))
 
-        if self.cfg_params.get(KEY_AUTH_TYPE, 'user/password') == 'user/password':
-            self.auth = tsc.TableauAuth(self.cfg_params[KEY_USER_NAME], self.cfg_params[KEY_API_PASS],
-                                        site_id=site_id)
-        elif self.cfg_params.get(KEY_AUTH_TYPE) == "Personal Access Token":
-            self.auth = tsc.PersonalAccessTokenAuth(token_name=self.cfg_params['token_name'],
-                                                    personal_access_token=self.cfg_params['#token_secret'],
-                                                    site_id=site_id)
+        client = TableauServerClient(
+            token_name=token_name,
+            token_secret=token_secret,
+            site_id=site_id,
+            endpoint=endpoint,
+        )
 
-        self.server = tsc.Server(self.cfg_params[KEY_ENDPOINT], use_server_version=True)
-        self.server_info = self.server.server_info.get()
-        logging.info(F"Using server API version: {self.server_info.rest_api_version}")
+        # all_datasources = client.get_datasources()
+        # print(all_datasources)
+        # all_tasks = client.get_tasks()
+        # print(all_tasks)
 
-    def run(self):
-        '''
-        Main execution code
-        '''
-        params = self.cfg_params  # noqa
+        refresh_specs = [
+            DatasourceRefreshSpec.from_dict(ds_refresh_spec)
+            for ds_refresh_spec in datasource_refresh_specs
+        ]
 
-        with self.server.auth.sign_in(self.auth):
-            executed_jobs = dict()
-
-            data_sources = params[KEY_DATASOURCES]
-            # tasks
-            # filter only datasource refresh tasks
-            logging.info('Validating extract names...')
-
-            all_ds, validation_errors = self._get_all_ds_by_filter(data_sources)
-            if validation_errors:
-                for err in validation_errors:
-                    logging.exception(err)
-                exit(1)
-            logging.debug(F'recognized datasets: {all_ds}')
-            ds_to_refresh = self.validate_dataset_names(all_ds, data_sources)
-
-            tasks = self.get_all_datasource_refresh_tasks()
-            # get all datasources for tasks
-            logging.info('Retrieving extract tasks and validating extract types...')
-            ds_tasks = self.get_all_ds_for_tasks(tasks, all_ds)
-            logging.debug(F"Found tasks: {ds_tasks}")
-            self.validate_dataset_types(ds_tasks, ds_to_refresh)
-
-            for ds in data_sources:
-                task = ds_tasks[ds[KEY_DS_NAME]][ds[KEY_DS_TYPE].lower()]
-                logging.info(F'Triggering extract for: "{ds[KEY_DS_NAME]}" with LUID: "{task.target.id}""')
-                job_id = self._run_task(task)
-                executed_jobs[ds[KEY_DS_NAME]] = job_id
-
-            # poll job statuses
-            if params.get(KEY_POLL_MODE):
-                logging.info('Polling extract refresh statuses.')
-                self._wait_for_finish(executed_jobs)
-
-        logging.info('Trigger finished successfully!')
-
-    def _run_task(self, task):
-        response = self.server.tasks.run(task)
-        root = xmltodict.parse(response)
-
-        job_id = root['tsResponse']['job']['@id']
-        return job_id
-
-    def get_all_datasource_refresh_tasks(self):
-        # filter only datasource refresh tasks
-        tasks = list(tsc.Pager(self.server.tasks))
-        return [task for task in tasks if task.target.type == 'datasource']
-
-    def validate_dataset_names(self, all_ds, datasources):
-        conf_ds_names = dict()
-        for ds in datasources:
-            conf_ds_names[ds['name']] = ds['type']
-        ds_names = [ds.name for ds in all_ds]
-        inv_names = [nm for nm in conf_ds_names if nm not in ds_names]
-        if inv_names:
-            raise ValueError(F'Some datasets do not exist! {inv_names}')
-        return conf_ds_names
-
-    def get_all_ds_for_tasks(self, tasks, all_ds):
-        ds_tasks = dict()
-        ds_ids = dict()
-        for ds in all_ds:
-            ds_ids[ds.id] = ds.name
-
-        for t in tasks:
-            if t.target.id not in ds_ids:
-                continue
-
-            ds = self.server.datasources.get_by_id(t.target.id)
-            # normalize increment task
-            ds_tasks[ds.name] = ds_tasks.get(ds.name, dict())
-            ds_tasks[ds.name][t.task_type.lower()] = t
-
-        return ds_tasks
-
-    def validate_dataset_types(self, ds_tasks, param):
-
-        inv_ds = [{ds: param[ds]} for ds in param if not ds_tasks.get(ds, {}).get(param[ds].lower())]
-
-        if inv_ds:
-            raise ValueError(F'Some datasets do not have the required refresh type task: {inv_ds}. '
-                             F'Please create the extract refresh of that type first.')
-
-    def _wait_for_finish(self, executed_jobs):
-        remaining_jobs = executed_jobs.copy()
-        failed_jobs = dict()
-        while remaining_jobs:
-            for ds_name in executed_jobs:
-                job = self.server.jobs.get_by_id(executed_jobs[ds_name])
-                if int(job.finish_code) >= 0:
-                    remaining_jobs.pop(ds_name, {})
-
-                    if int(job.finish_code) > 0:  # job failed
-                        failed_jobs[ds_name] = job
-
-        if failed_jobs:
-            raise RuntimeError(F'Some jobs did not finish properly: {failed_jobs}')
-
-    def _get_all_ds_by_filter(self, data_sources):
-        all_ds = list()
-        validation_errors = list()
-        for ds_filter in data_sources:
-            # if luid specified get the source
-            if ds_filter.get(KEY_LUID):
-                res = self.server.datasources.get_by_id(ds_filter[KEY_LUID])
-                ds = [res] if res else []
-
-            else:
-                ds = self._get_all_datasources_by_filter(ds_filter[KEY_NAME], ds_filter.get(KEY_TAG))
-            all_ds.extend(ds)
-            err = self._validate_ds_result(ds_filter, ds)
-            if err:
-                validation_errors.append(err)
-
-        return all_ds, validation_errors
-
-    def _str_ds(self, ds_arr):
-        str = '['
-        for ds in ds_arr:
-            str += F'(Name: {ds.name}, Project:{ds.project_name}, LUID: {ds.id}, tags: {ds.tags}), '
-        str += ']'
-        return str
-
-    def _validate_ds_result(self, filter, ds):
-        ds_error = None
-        if not ds and not filter.get(KEY_LUID):
-            ds_error = F'There is no result for combination of name & tag {filter}'
-        if not ds and filter.get(KEY_LUID):
-            ds_error = F'There is no result for specified LUID, the datasource does not exist {filter[KEY_LUID]}'
-
-        # this happens when luid is set and name is not matching the dataset
-        if len(ds) == 1 and filter[KEY_NAME] != ds[0].name:
-            ds_error = F"The dataset name retrieved by the specified LUID: '{ds[0].name}' " \
-                       F"does not match the '{filter[KEY_NAME]}' specified in corresponding filter: {filter}"
-
-        if len(ds) > 1:
-            ds_error = F"There is more results for given filter: {filter}, " \
-                       F"set more specific tag or use LUID. The results are: {self._str_ds(ds)}"
-        return ds_error
-
-    def _get_all_datasources_by_filter(self, name, tag):
-        req_option = tsc.RequestOptions()
-        req_option.filter.add(tsc.Filter(tsc.RequestOptions.Field.Name,
-                                         tsc.RequestOptions.Operator.Equals,
-                                         name))
-        if tag:
-            req_option.filter.add(tsc.Filter(tsc.RequestOptions.Field.Tags,
-                                             tsc.RequestOptions.Operator.Equals,
-                                             tag))
-        datasource_items = list(tsc.Pager(self.server.datasources, req_option))
-        return datasource_items
+        client.refresh_datasources(refresh_specs, wait_for_jobs=wait_for_jobs)
 
 
 """
         Main entrypoint
 """
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        debug = sys.argv[1]
-    else:
-        debug = False
     try:
-        comp = Component(debug)
-        comp.run()
-    except Exception as e:
-        logging.exception(e)
+        comp = TableauExtractRefreshTrigger()
+        # this triggers the run method by default and is controlled by the configuration.action parameter
+        comp.execute_action()
+    except UserException as exc:
+        logging.exception(exc)
         exit(1)
+    except Exception as exc:
+        logging.exception(exc)
+        exit(2)
